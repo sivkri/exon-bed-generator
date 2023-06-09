ccc3b7aa0b5d86938c4803008e8aaeccb1830c0b
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-09 13:46:07 +0200

    Update create_exon_bed.sh

--- create_exon_bed.sh
+++ create_exon_bed.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-wget ftp://ftp.ensembl.org/pub/release-76/gtf/homo_sapiens/Homo_sapiens.GRCh38.76.gtf.gz
-zcat Homo_sapiens.GRCh38.76.gtf.gz \
-| grep -w exon | tr '"' '\t' \
-| cut -f1,4,5,10,16 | sed 's/\t/_/4' \
-| sort -k4 > tmp.bed
 
-mkdir Homo_sapiens.GRCh38_explode
+# Download the GTF file
+wget -O Homo_sapiens.GRCh38.76.gtf.gz ftp://ftp.ensembl.org/pub/release-76/gtf/homo_sapiens/Homo_sapiens.GRCh38.76.gtf.gz
+
+# Extract the GTF file
+zcat Homo_sapiens.GRCh38.76.gtf.gz \
+  | grep -w exon \
+  | awk -F '\t' 'BEGIN{OFS="\t"}{gsub(/"/, "", $10); print $1, $4, $5, $10, $16}' \
+  | sort -k4 > tmp.bed
+
+# Create the output directory
+mkdir -p Homo_sapiens.GRCh38_explode
 cd Homo_sapiens.GRCh38_explode
+
+# Split the BED file into separate files based on column 4
 awk '{print > $4}' ../tmp.bed
 
-for i in `ls ENS*`
-do cat $i | sortBed | mergeBed -nms \
-| cut -d ';' -f1
-done | sortBed > ../Homo_sapiens.GRCh38_exon.bed
+# Process each file and merge the results
+for file in ENS*
+do
+  sortBed $file | mergeBed -nms \
+    | awk -F ';' '{print $1}' \
+    >> ../Homo_sapiens.GRCh38_exon.bed
+done
+
+# Sort the final output
+sortBed -o ../Homo_sapiens.GRCh38_exon.bed ../Homo_sapiens.GRCh38_exon.bed
+
+# Clean up temporary files
+rm ../tmp.bed
